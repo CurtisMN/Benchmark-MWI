ac54b465302d495a92da172efdde9c15fbb8a3e7
Curtis Nollner <curtis@inquisitivedevelopment.com>
2019-02-02 20:32:19 -0600

    Finish the javascript test.

--- src/components/contactform.js
+++ src/components/contactform.js
@@ -5,21 +5,21 @@ import '../components/contactform.css';
 
 export const ContactForm = () => (
   <Fragment>
-    <div className='contactwrapper' style={{paddingBottom: '2%'}}>
-    <h2 className='contactheader'>CONTACT</h2>
+    <div className='contactwrapper' style={{ paddingBottom: '2%' }}>
+      <h2 className='contactheader'>CONTACT</h2>
       <div className='contactform'>
-        <form action='' method="POST">
+        <form action='' method='POST'>
           Email
           <br />
           <input type='email' name='email' pattern='*@*.' required />
           <br />
           Subject
           <br />
-          <input type='text' name='subject' required/>
+          <input type='text' name='subject' required />
           <br />
           Message
           <br />
-          <textarea type='textarea' rows='8' required/>
+          <textarea type='textarea' rows='8' required />
           <button>Submit</button>
         </form>
       </div>
--- src/components/jstest.js
+++ src/components/jstest.js
@@ -1,19 +1,59 @@
-import React, { Fragment } from 'react';
+/* eslint-disable */
+
+import React, { Fragment, Component } from 'react';
 
 import '../components/jstest.css';
 
-export const JsTest = () => (
-  <Fragment>
-    <h1>HEADING ONE</h1>
-    <div className='statement'>
-      Remove the duplicates in 2 Javascript objects and output the list of distinct names in an
-      unordered list when this{' '}
-      <a className='actiavtionlink' href='/index/'>
-        link is clicked
-      </a>
-      , if the operation has been completed already notify the user that this has already been done.
-    </div>
-  </Fragment>
-);
+// Create the two arrays, and an empty array
+var nameListOne = ['Matt Johnson', 'Bart Paden', 'Ryan Doss', 'Michael Spangler'];
+var nameListTwo = ['Matt Johnson', 'Bart Paden', 'Jordan Heigle', 'Tyler Viles'];
+var nameListUnique = [];
+
+//Go through each item in the arrays, adding any unique name to the unique item array
+nameListOne.forEach((name) => {
+  if (!nameListUnique.includes(name)) nameListUnique.push(name);
+});
+nameListTwo.forEach((name) => {
+  if (!nameListUnique.includes(name)) nameListUnique.push(name);
+});
+
+const displayListUnique = nameListUnique.map((name) => <li>{name}</li>);
+
+class JsTest extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hide: true,
+    };
+  }
+  showList(event) {
+    this.setState({
+      hide: false,
+    });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <h1>HEADING ONE</h1>
+        <div className='statement'>
+          Remove the duplicates in 2 Javascript objects and output the list of distinct names in an
+          unordered list when this{' '}
+          <a
+            className='activationlink'
+            href='javascript: document.body.scrollIntoView(false);'
+            id='switch'
+            onClick={this.showList.bind(this)}
+          >
+            link is clicked
+          </a>
+          , if the operation has been completed already notify the user that this has already been
+          done.
+        </div>
+        <div className={this.state.hide ? 'hidden' : 'shown'}>{displayListUnique}</div>
+      </Fragment>
+    );
+  }
+}
 
 export default JsTest;
